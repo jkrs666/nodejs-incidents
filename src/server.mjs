36fe95491e108656bef51fe6ea632d3063f0c745
Aristides Kodjas <rskj@proton.me>
2025-12-21 17:21:30 +0200

    refactor router

--- src/server.mjs
+++ src/server.mjs
@@ -3,13 +3,49 @@ import { validateIncident, getBody } from './utils.mjs'
 
 const incidents = [
 	{
-		id: 'qwer',
+		id: 'a1101818-cb9e-4631-92c1-69a045778aa3',
 		title: 'test',
 		severity: 'low',
 		status: 'open',
 		createdAt: 1777777777777
 	}
 ]
+
+const createIncident = async (req, res) => {
+	const incident = await getBody(req)
+	try {
+		validateIncident(incident)
+	} catch (err) {
+		res.statusCode = 400
+		return { error: `invalid request. ${err.message}` }
+	}
+	incidents.push(incident)
+	return { message: `incident ${incident.id} inserted` }
+}
+
+const updateIncident = async (req, res, id) => {
+	const patchBody = await getBody(req)
+	const invalidFields = Object.keys(patchBody).filter(k => !['title', 'severity', 'status', 'createdAt'].includes(k))
+	if (invalidFields.length > 0) {
+		return { 'error': `invalid fields: ${invalidFields}` }
+	}
+
+	const i = incidents.findIndex(i => i.id === id)
+	if (i == -1) {
+		res.statusCode = 404
+		return { error: `incident ${id} not found` }
+	}
+
+	incidents[i] = { ...incidents[i], ...patchBody }
+	try {
+		validateIncident(incidents[i])
+	} catch (err) {
+		res.statusCode = 400
+		return { error: `invalid request. ${err.message}` }
+	}
+
+	return m
+}
 
 const server = createServer(async (req, res) => {
 	res.statusCode = 200
@@ -19,28 +55,12 @@ const server = createServer(async (req, res) => {
 		let m = endpoint.match('PATCH /incidents/([a-z0-9-]+)')
 		if (m !== null) {
 			let [_, id] = m
-			console.log(m)
-			console.log(id)
-			const patchBody = await getBody(req)
-			const i = incidents.findIndex(i => i.id === id)
-			incidents[i] = { ...incidents[i], ...patchBody }
-			console.log(incidents)
-			return m
+			return await updateIncident(req, res, id)
 		}
 
 		switch (endpoint) {
 			case 'GET /incidents': return incidents
-			case 'POST /incidents': {
-				const incident = await getBody(req)
-				try {
-					validateIncident(incident)
-				} catch (err) {
-					res.statusCode = 400
-					return { error: `invalid request. ${err.message}` }
-				}
-				incidents.push(incident)
-				return { message: 'incident inserted' }
-			}
+			case 'POST /incidents': return createIncident(req, res)
 			default: {
 				res.statusCode = 404
 				return { error: 'not found' }
